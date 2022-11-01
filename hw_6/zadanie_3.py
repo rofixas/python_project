d756ede8786b64fa3fe472390ebaa481ecf0703e
rofixas <rofixassss@gmail.com>
2022-11-01 22:28:21 +0300

    Пирамида

--- hw_6/zadanie_3.py
+++ hw_6/zadanie_3.py
@@ -1,8 +1,15 @@
-h = int(input())
-while 3 <= h <= 9:
-    for i in range(1,h):
-        for j in range(1,i):
-            print(j, end=' ')
-
-        print(i)
+# h = int(input())
+# while 3 <= h <= 9:
+#     for i in range(1,h):
+#         for j in range(1,i):
+#             print(j, end=' ')
+#
+#         print(i)
+#     break
+s = input()
+l = len(s) // 2
+while 3 <= l <= 9:
+    for i in range(l, -1, -1):
+        print(' ' * i, end=' ')
+        print(s[i:-i - 1])
     break
