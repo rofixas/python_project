d8cb7ebc65b3ef6820d8b7e7666ac24b2c36db8f
rofixas <rofixassss@gmail.com>
2022-11-03 10:37:48 +0300

    Подсчет количества одинаковых символов

--- hw_6/zadanie_2.py
+++ hw_6/zadanie_2.py
@@ -1,5 +1,5 @@
-stroka = input()
-char = input()
+stroka = input('Введите строку в которой нужно искать символы: ')
+char = input('Введите символ который нужно найти в строке: ')
 cnt = 0
 b = -1
 while True:
