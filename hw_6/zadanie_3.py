b26fddbab4f784b8c89951f54bc5f4feac4ba0d1
rofixas <rofixassss@gmail.com>
2022-11-04 19:54:09 +0300

    Пирамида

--- hw_6/zadanie_3.py
+++ hw_6/zadanie_3.py
@@ -1,15 +1,6 @@
-# h = int(input())
-# while 3 <= h <= 9:
-#     for i in range(1,h):
-#         for j in range(1,i):
-#             print(j, end=' ')
-#
-#         print(i)
-#     break
-s = input('Введите символы из которых должна состоять пирамида : ')
-l = len(s) // 2
-while 3 <= l <= 9:
-    for i in range(l, -1, -1):
-        print(' ' * i, end=' ')
-        print(s[i:-i - 1])
-    break
+num = input('Введите символы для пирамиды : ')
+n = int(input('Введите высоту пирамиды : '))
+
+for i in range(n):
+    b = num[i::-1]
+    print(b[::-1])
