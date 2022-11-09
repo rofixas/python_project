babde271dae42de207243c75d9a70c45ec31de92
rofixas <rofixassss@gmail.com>
2022-11-09 18:43:32 +0300

    Одинаковые элементы списка

--- hw_8/zadanie_3.py
+++ hw_8/zadanie_3.py
@@ -1,11 +1,14 @@
 a = [1, 2, 5, 7, 9, 99, 87, 200, 39, 2]
 b = [5, 42, 29, 345, 50, 33, 7, 0, 201, 9, 2, 132, 45, 23,934]
 c = []
+
 for i in a:
-    for j in b:
-        if i == j:
-            c.append(i)
+    if i in b and i not in c:
+        c.append(i)
 
 print(c)
+for j in c:
+    g = c.count(j)
+    print('Элемент списка :' , j , 'Встречается ' , g , ' раз')
 
 
