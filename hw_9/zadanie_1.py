342d96246d9e1aeeedd1313cef1267ac1a7a7db4
rofixas <rofixassss@gmail.com>
2022-11-13 14:06:17 +0300

    Построение матрицы

---
+++ hw_9/zadanie_1.py
@@ -0,0 +1,8 @@
+n = int(input('Введите размер матрицы матрицы : '))
+m = [
+    [j for i in range(n)] if j % 2 == 0 else [i for i in range(-n, 0)]
+    for j in range(n)
+]
+
+for i in m:
+    print(i)
