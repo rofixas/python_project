472c62e251fb028b0f36671eb8e4ced3493117ca
rofixas <rofixassss@gmail.com>
2022-11-13 14:07:34 +0300

    Создание матрицы

--- hw_9/zadanie_1.py
+++ hw_9/zadanie_1.py
@@ -1,4 +1,4 @@
-n = int(input('Введите размер матрицы матрицы : '))
+n = int(input('Введите размер матрицы : '))
 m = [
     [j for i in range(n)] if j % 2 == 0 else [i for i in range(-n, 0)]
     for j in range(n)
