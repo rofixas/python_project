3e823dce25c097b131a1c7ddaff77415ae2d7c5a
rofixas <rofixassss@gmail.com>
2022-11-13 14:42:16 +0300

    Четные и нечетные числа списка

---
+++ hw_9/zadanie_3.py
@@ -0,0 +1,16 @@
+import random
+sumchet = 0
+sumnechet = 0
+l = [random.randint(1, 15) for i in range(1, 16)]
+print(l)
+for i in range(len(l)):
+    if l[i] % 2 == 0:
+        sumchet += l[i]
+    if l[i] % 2 != 0:
+        sumnechet += l[i]
+print('Сумма четных чисел списка : ', sumchet)
+print('Сумма не четных чисел списка : ', sumnechet)
+if sumchet > sumnechet:
+    print("Сумма четных чисел списка больше")
+else:
+    print('Сумма нечетных чисел списка больше')
