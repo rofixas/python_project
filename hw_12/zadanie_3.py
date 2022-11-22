57fbd80a65d99488a8a92bf42e9cfb56a60387b3
rofixas <rofixassss@gmail.com>
2022-11-22 20:17:39 +0300

    Функция def

--- hw_12/zadanie_3.py
+++ hw_12/zadanie_3.py
@@ -4,11 +4,11 @@ lst = []
 def gen(n, z):
 
     for i in range(n, z+1):
-        k = 0
+        cnt = 0
         for j in range(1, i+1):
             if i % j == 0:
-                k += 1
-        if k == 2:
+                cnt += 1
+        if cnt == 2:
             lst.append(i)
     return lst
 
